28a2aba7eacb6eda07adcd2bfe3140cbb765c62e
tsigel <tsddaniil@gmail.com>
2024-12-14 14:58:10 +0100

    Fix async-map

--- packages/async-map/asyncMap.spec.ts
+++ packages/async-map/asyncMap.spec.ts
@@ -1,72 +1,98 @@
-import { asyncMap } from './asyncMap';
+import { asyncMap } from "./asyncMap";
 
-describe('Async map', () => {
+describe("Async map", () => {
+  it("Check chain call", (done) => {
+    return asyncMap(
+      1,
+      (item) => {
+        return Promise.resolve(item);
+      },
+      [1, 2, 3]
+    )
+      .then((list) => {
+        expect(list).toEqual([1, 2, 3]);
+      })
+      .then(done);
+  });
 
-    it('Check chain call', (done) => {
-        return asyncMap(1, (item) => {
-            return Promise.resolve(item);
-        }, [1, 2, 3])
-            .then((list) => {
-                expect(list).toEqual([1, 2, 3]);
-            }).then(done);
+  it("Curry call", (done) => {
+    const mapTo = asyncMap(1, (item: number) => {
+      return item * 2 + 1;
     });
-
-    it('Check all treads', (done) => {
-        return asyncMap(5, (item) => {
-            return new Promise((resolve) => {
-                setTimeout(() => resolve(item), (5 - item) * 10);
-            });
-        }, [1, 2, 3, 4, 5])
-            .then((list) => {
-                expect(list).toEqual([1, 2, 3, 4, 5]);
-            }).then(done);
+    mapTo([1, 2, 3]).then((result) => {
+      expect(result).toEqual([3, 5, 7]);
+      done();
     });
+  });
 
-    it('Not promise return', (done) => {
-        return asyncMap(1, (item) => ({ [item]: item }), [1, 2, 3])
-            .then((list) => {
-                expect(list).toEqual([{ 1: 1 }, { 2: 2 }, { 3: 3 }]);
-            }).then(done);
-    });
+  it("Check all treads", (done) => {
+    return asyncMap(
+      5,
+      (item) => {
+        return new Promise((resolve) => {
+          setTimeout(() => resolve(item), (5 - item) * 10);
+        });
+      },
+      [1, 2, 3, 4, 5]
+    )
+      .then((list) => {
+        expect(list).toEqual([1, 2, 3, 4, 5]);
+      })
+      .then(done);
+  });
 
-    it('Run with exception in callback function', (done) => {
-        return asyncMap(1, (item) => {
-            if (item === 3) {
-                throw new Error('Check exception');
-            }
-            return item;
-        }, [1, 2, 3])
-            .then(
-                () => {
-                    throw new Error('Wrong test work!');
-                },
-                (e) => {
-                    expect(e.message).toBe('Check exception');
-                    done();
-                }
-            );
-    });
+  it("Not promise return", (done) => {
+    return asyncMap(1, (item) => ({ [item]: item }), [1, 2, 3])
+      .then((list) => {
+        expect(list).toEqual([{ 1: 1 }, { 2: 2 }, { 3: 3 }]);
+      })
+      .then(done);
+  });
 
-    it('Check long async array', (done) => {
-        const list: Array<number> = [];
-
-        for (let i = 0; i < 100; i++) {
-            list.push(i);
+  it("Run with exception in callback function", (done) => {
+    return asyncMap(
+      1,
+      (item) => {
+        if (item === 3) {
+          throw new Error("Check exception");
         }
+        return item;
+      },
+      [1, 2, 3]
+    ).then(
+      () => {
+        throw new Error("Wrong test work!");
+      },
+      (e) => {
+        expect(e.message).toBe("Check exception");
+        done();
+      }
+    );
+  });
 
-        function randomInteger(min: number, max: number): number {
-            const rand = min - 0.5 + Math.random() * (max - min + 1);
-            return Math.round(rand);
-        }
+  it("Check long async array", (done) => {
+    const list: Array<number> = [];
 
-        return asyncMap(5, (item) => {
-            return new Promise(resolve => {
-                setTimeout(() => resolve(item), randomInteger(0, 100));
-            });
-        }, list)
-            .then(result => {
-                expect(result).toEqual(list);
-                done();
-            });
+    for (let i = 0; i < 100; i++) {
+      list.push(i);
+    }
+
+    function randomInteger(min: number, max: number): number {
+      const rand = min - 0.5 + Math.random() * (max - min + 1);
+      return Math.round(rand);
+    }
+
+    return asyncMap(
+      5,
+      (item) => {
+        return new Promise((resolve) => {
+          setTimeout(() => resolve(item), randomInteger(0, 100));
+        });
+      },
+      list
+    ).then((result) => {
+      expect(result).toEqual(list);
+      done();
     });
+  });
 });
--- packages/async-map/asyncMap.ts
+++ packages/async-map/asyncMap.ts
@@ -1,53 +1,76 @@
-export const asyncMap = <T, U>(treads: number, callback: (data: T, index?: number) => U | Promise<U>, list: Array<T>): Promise<Array<U>> => {
-    const isPromise = (item: unknown): item is Promise<unknown> =>
-        typeof item === 'object' && typeof (item as Promise<unknown>)?.then === 'function';
+const map = <T, U>(
+  treads: number,
+  callback: (data: T, index: number) => U | Promise<U>,
+  list: Array<T>
+): Promise<Array<U>> => {
+  const isPromise = (item: unknown): item is Promise<unknown> =>
+    typeof item === "object" &&
+    typeof (item as Promise<unknown>)?.then === "function";
 
-    const local = list.slice().reverse();
-    const result: Array<U> = [];
-    let activeTreads = 0;
+  const local = list.slice().reverse();
+  const result: Array<U> = [];
+  let activeTreads = 0;
 
-    return new Promise((resolve, reject) => {
-        const loop = () => {
-            const index = list.length - local.length;
+  return new Promise((resolve, reject) => {
+    const loop = () => {
+      const index = list.length - local.length;
 
-            const addItem = (index: number) => (item: U) => {
-                result[index] = item;
-                activeTreads--;
-                loop();
-            };
-
-            if (!local.length) {
-                if (activeTreads === 0) {
-                    resolve(result);
-                }
-                return void 0;
-            }
-
-            const next = local.pop() as T;
-            activeTreads++;
-
-            try {
-                const localResult = callback(next, index);
-
-                if (isPromise(localResult)) {
-                    localResult
-                        .then(addItem(index))
-                        .catch(reject);
-                } else {
-                    Promise.resolve(localResult)
-                        .then(addItem(index))
-                        .catch(reject);
-                }
-            } catch (e) {
-                reject(e);
-                return void 0;
-            }
-
-            if (activeTreads < treads) {
-                loop();
-            }
-        };
-
+      const addItem = (index: number) => (item: U) => {
+        result[index] = item;
+        activeTreads--;
         loop();
-    });
+      };
+
+      if (!local.length) {
+        if (activeTreads === 0) {
+          resolve(result);
+        }
+        return void 0;
+      }
+
+      const next = local.pop() as T;
+      activeTreads++;
+
+      try {
+        const localResult = callback(next, index);
+
+        if (isPromise(localResult)) {
+          localResult.then(addItem(index)).catch(reject);
+        } else {
+          Promise.resolve(localResult).then(addItem(index)).catch(reject);
+        }
+      } catch (e) {
+        reject(e);
+        return void 0;
+      }
+
+      if (activeTreads < treads) {
+        loop();
+      }
+    };
+
+    loop();
+  });
 };
+
+export function asyncMap<T, U>(treads: number, cb: Cb<T, U>): Curry1<T, U>;
+export function asyncMap<T, U>(
+  treads: number,
+  cb: Cb<T, U>,
+  list: T[]
+): Promise<U[]>;
+export function asyncMap<T, U>(
+  treads: number,
+  callback: Cb<T, U>,
+  list?: Array<T>
+): Promise<Array<U>> | Curry1<T, U> {
+  if (!list) {
+    return (list: Array<T>) => map(treads, callback, list);
+  }
+  return map(treads, callback, list);
+}
+
+type Curry1<T, U> = (list: Array<T>) => Promise<Array<U>>;
+type Cb<T, U> = (data: T, index: number) => U | Promise<U>;
+
+export default asyncMap;
